7b98f5739e6a6137daaa3b08d42803352d7b519f
g24ai1022 <g24ai1022@iitj.ac.in>
2025-07-30 22:01:20 +0530

    updated python files with directory name

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ def load_data():
     X, y = fetch_california_housing(return_X_y=True)
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
-def train_and_save_model(model_path="model.joblib"):
+def train_and_save_model(model_path="models/model.joblib"):
     X_train, X_test, y_train, y_test = load_data()
     model = LinearRegression()
     model.fit(X_train, y_train)
