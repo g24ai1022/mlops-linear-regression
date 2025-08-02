e65f03b4e06e22aa68ca311915c0985e648953fe
g24ai1022 <g24ai1022@iitj.ac.in>
2025-08-02 23:24:46 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -3,7 +3,7 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 
 def main():
-    model = joblib.load("artifacts/model.joblib")
+    model = joblib.load("model.joblib")
     X, y = fetch_california_housing(return_X_y=True)
     _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
